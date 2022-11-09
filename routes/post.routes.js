b5f21a63054f476dcdbe9ee2c929737f86c38af7
andrew-cmdltt <menwhohas2279@gmail.com>
2022-11-09 15:36:06 +0300

    node postgres

---
+++ routes/post.routes.js
@@ -0,0 +1,8 @@
+const Router = require("express")
+const router = new Router()
+const postController = require("../controllers/post.controllers")
+
+router.post("/post", postController.createPost)
+router.get("/post", postController.getPostByUser)
+
+module.exports = router 
